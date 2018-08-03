2f0c45a01041a1f99eeae399a8990f61fea642fb
lyricly <934991794@qq.com>
2018-08-03 15:23:19 +0800

    init

---
+++ app/auth/routes.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from flask import render_template, url_for, flash, redirect, request, session, abort
+from flask_login import login_user, current_user, logout_user, login_required
+from .forms import RegistrationForm, LoginForm
+from app import bcrypt, db
+from app.models import User
+from . import auth
+
+
+@auth.route('/')
+def index():
+    return render_template('index.html')
+
+@auth.route("/admin/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    # if form.validate_on_submit():
+    #     hashed_pwd = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+    #     user = User(username=form.username.data, email=form.email.data, password=hashed_pwd)
+    #     db.session.add(user)
+    #     db.session.commit()
+    #     return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', title='注册', form=form)
+
+
+@auth.route("/login", methods=['GET', 'POST'])
+def login():
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('main.home'))
+    form = LoginForm()
+    # if form.validate_on_submit():
+    #     user = User.query.filter_by(username=form.username.data).first()
+    #     if user and bcrypt.check_password_hash(user.password, form.password.data):
+    #         login_user(user, remember=form.remember.data)
+    #         next_page = request.args.get('next')
+    #         if next_page:
+    #             return redirect(next_page)
+    #         else:
+    #             return redirect(url_for('main.home'))
+    #     else:
+    #         flash('登录失败，请检查账号或者密码', 'danger')
+    return render_template('auth/login.html', title='登录', form=form)
+
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
+
---
+++ app/models.py
@@ -0,0 +1,26 @@
+from . import db
+from datetime import datetime
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image = db.Column(db.String(40), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.String(64), nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    content = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    category = db.Column(db.Integer, nullable=False, default=0)
+    keyword = db.Column(db.String(64), nullable=True)
+    post_type = db.Column(db.Integer, nullable=False)
